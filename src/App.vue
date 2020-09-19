f27c0a935ec89780a423304dcffb4569125738f7
MasterEvarior <masterevarior@protonmail.com>
2020-09-19 13:09:44 +0200

    Added side drawer

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,57 @@
 <template>
   <div id="app">
     <v-app>
-      <v-app-bar max-height="7%">
+      <v-navigation-drawer app clipped v-model="drawer">
+        <v-list>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-account</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>My Account</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-view-dashboard</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>My Encounters</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+          <v-list-item link>
+            <v-list-item-action>
+              <v-icon>mdi-cog</v-icon>
+            </v-list-item-action>
+            <v-list-item-content>
+              <v-list-item-title>Settings</v-list-item-title>
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-navigation-drawer>
+      <v-app-bar app clipped-left max-height="7%">
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
         <v-toolbar-title>Encounter-Tavern</v-toolbar-title>
       </v-app-bar>
-      <router-view/>
+      <v-main>
+        <router-view />
+      </v-main>
       <v-footer app>
         <span>&copy; {{ new Date().getFullYear() }}</span>
       </v-footer>
     </v-app>
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+    };
+  },
+};
+</script>
 
 <style lang="scss">
 #app {
